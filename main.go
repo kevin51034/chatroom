3ab0619ec23e8ad59fdbcf546f92fab5a6faa721
陳文擎 <kevin51034@gmail.com>
2020-11-06 11:21:29 +0800

    implement client and hub

--- main.go
+++ main.go
@@ -9,28 +9,29 @@ import (
 )
 
 var tpl *template.Template
-var upgrader = websocket.Upgrader{
-    ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
-
 
 func init() {
 	tpl = template.Must(template.ParseGlob("public/*.html"))
 }
 
 func main() {
+	// create hub
+	hub := newHub()
+	go hub.run()
+
 	http.HandleFunc("/", index)
+	/*http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})*/
 	http.HandleFunc("/ws", wsEndPoint)
-	//http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("public"))))
-	http.Handle("/public/css/", http.StripPrefix("/public/css", http.FileServer(http.Dir("public/css"))))
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+	//http.Handle("/public/css/", http.StripPrefix("/public/css", http.FileServer(http.Dir("public/css"))))
+	//http.Handle("/public/js/", http.StripPrefix("/public/js", http.FileServer(http.Dir("public/js"))))
 
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("home")
 	tpl.ExecuteTemplate(w, "index.html", nil)
 }
 
@@ -42,7 +43,8 @@ func reader(conn *websocket.Conn) {
 			return
 		}
 		fmt.Println(string(p))
-		if err := conn.WriteMessage(messageType, p); err != nil {
+		reply := string(p) + " receive message from client and send it back!"
+		if err := conn.WriteMessage(messageType, []byte(reply)); err != nil {
 			log.Println(err)
 			return
 		}
--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,9 @@ const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true
 });
 
+console.log(username)
+console.log(room)
+
 const socket = io();
 
 // Join chatroom
@@ -21,20 +24,52 @@ socket.on('roomUsers', ({ room, users }) => {
 
 // Message from server
 socket.on('message', message => {
-  console.log(message);
+  //console.log(message);
   outputMessage(message);
 
   // Scroll down
   chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
+// *****
+if (window["WebSocket"]) {
+  conn = new WebSocket("ws://" + document.location.host + "/ws");
+  console.log("build connection")
+  outputRoomName(room);
+  outputUsers(users);
+  conn.onclose = function (evt) {
+            /*var item = document.createElement("div");
+            item.innerHTML = "<b>Connection closed.</b>";
+            appendLog(item);*/
+            console.log("Connection close")
+  };
+  conn.onmessage = function (evt) {
+      var messages = evt.data.split('\n');
+      /*
+      for (var i = 0; i < messages.length; i++) {
+          var item = document.createElement("div");
+          item.innerText = messages[i];
+          appendLog(item);
+      }*/
+      outputMessage(messages)
+      console.log(messages)
+  };
+} else {
+  /*var item = document.createElement("div");
+  item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
+  appendLog(item);*/
+  console.log("Your browser does not support WebSockets.")
+}
+// *****
+
 // Message submit
 chatForm.addEventListener('submit', e => {
   e.preventDefault();
-
   // Get message text
   let msg = e.target.elements.msg.value;
-  
+  console.log(msg)
+  conn.send(msg);
+
   msg = msg.trim();
   
   if (!msg){
@@ -73,9 +108,12 @@ function outputRoomName(room) {
 // Add users to DOM
 function outputUsers(users) {
   userList.innerHTML = '';
-  users.forEach(user=>{
+  /*users.forEach(user=>{
     const li = document.createElement('li');
     li.innerText = user.username;
     userList.appendChild(li);
-  });
- }
+  });*/
+  const li = document.createElement('li');
+  li.innerText = username;
+  userList.appendChild(li);
+}
